7b6693a23c4442dfc73692626acd106f715b706f
raviranjanmahto1 <128248056+raviranjanmahto1@users.noreply.github.com>
2024-01-12 16:43:20 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const authRoutes = require("./routes/userRoute");
+const errorGlobalMiddleware = require("./middlewares/errorMiddleware");
+const AppError = require("./utils/appError");
+
+const app = express();
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static file
+app.use(express.static(path.join(__dirname, "dist")));
+
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => console.log(`Database connected successful🥰💚🥰`))
+  .catch(err =>
+    console.log(`Error connecting database 🎇💣💣💣🎇=>`, err.message)
+  );
+
+app.use("/api/v1/auth", authRoutes);
+
+app.all("*", (req, res, next) =>
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+);
+
+app.use(errorGlobalMiddleware);
+
+const port = process.env.PORT || 7007;
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
